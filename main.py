81cef3d2a27e61796de844e0ec13ba9733d0411d
DrWhats <hudorojckov.leva@gmail.com>
2022-11-27 19:09:08 +0500

    Update: распихал всё по методам

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from transformers import pipeline
 
-
 classifier = pipeline("zero-shot-classification", model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli")
 
-sequence_to_classify = "Добрый день. Возник вопрос по поводу учетной записи студента. При попытке зайти в Microsoft Teams выходит данное сообщение. Логин и пароль от учетной записи были присланы в понедельник, 14.11"
-candidate_labels = ["Учетная запись", "РПД", "Учебные планы", "Личный кабинет"]
-output = classifier(sequence_to_classify, candidate_labels, multi_label=False)
-print(output["labels"][0], output["scores"][0])
+def classify(text):
+    candidate_labels = ["Учетная запись", "РПД", "Учебные планы", "Личный кабинет"]
+    output = classifier(text, candidate_labels, multi_label=False, use_fast=False)
+    return output["labels"][0], output["scores"][0]
+def main():
+    print(classify("Как сменить пароль от личного кабинета?"))
 
-
+if __name__ == "__main__":
+    main()
 
