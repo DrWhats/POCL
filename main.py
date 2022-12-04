050629c714f5cfc150bedb93b92e1a4484adcaf8
Dr_Wat <hudorojckov.leva@gmail.com>
2022-12-04 21:52:05 +0500

    update

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ import streamlit as st
 classifier = pipeline("zero-shot-classification", model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli")
 
 
-text_input=st.text_input('Введите текст')
+text_input = st.text_input('Введите текст')
 
 
 def classify(text):
     candidate_labels = ["Учетная запись", "РПД", "Учебные планы", "Личный кабинет"]
     output = classifier(text, candidate_labels, multi_label=False, use_fast=False)
     return output["labels"][0], output["scores"][0]
-def main():
-    print(classify("Как сменить пароль от личного кабинета?"))
 
-if __name__ == "__main__":
-    main()
 
