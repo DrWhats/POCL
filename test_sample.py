d75ff79283805ca570f13c10a861712e3351b473
DrWhats <hudorojckov.leva@gmail.com>
2023-01-10 18:24:57 +0500

    UPDATE: добавлены тесты

--- test_sample.py
+++ test_sample.py
@@ -23,5 +23,11 @@ def test_classify():
     answer=classify("Добрый вечер! Не могу зайти в личный кабинет. Предполагаю, что я не зарегистрированный пользователь. Помогите, пожалуйста, разобраться.?")
     assert answer[1]>0.8 and answer[0]=='Учетная запись'
 
-# def test_status():
-#     assert requests.get('http://localhost:8501').status_code == 200
+def test_classify2():
+    answer=classify("Здравствуйте! Не могу понять как правильно сделать рабочую программу дисциплины в программе, как мне это сделать?")
+    assert answer[1]>0.9 and answer[0]=='Учебные планы'
+
+def test_classify3():
+    answer=classify("Психологический, группа OZ1121 Добрый день! Обращаюсь к вам с просьбой о создании корпоративной почты, с целью дальнейшей работы в системе Moodle и Teams.")
+    assert answer[1]>0.7 and answer[0]=='Учетная запись'
+
