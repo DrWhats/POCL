8d72a6efdc4815c0a0d15b99b595924cdf862ea2
DrWhats <hudorojckov.leva@gmail.com>
2023-04-09 21:57:51 +0500

    UPDATE added dockerfile

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 from transformers import pipeline
 import streamlit as st
+
 # import pytest
 classifier = pipeline("zero-shot-classification", model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli")
 
-
 text_input = st.text_input('Введите текст')
 
-def classify(text = None):
+
+def classify(text=None):
+    """Classify the given text using a classifier"""
     if text == None:
-        text = text_input
+        user_input = input("Please enter some text to classify: ")
+        text = user_input
     candidate_labels = ["Учетная запись", "РПД", "Учебные планы", "Личный кабинет"]
     output = classifier(text, candidate_labels, multi_label=False, use_fast=False)
     return output["labels"][0], output["scores"][0]
 
+
 submit = st.button('Отправить')
 
 if submit:
-   st.write(classify())
+    st.write(classify())
+
 
 # def test_classify():
 #     assert classify("Добрый день! Не могу войти в свой аккаунт. Что делать?") == ('Учетная запись', 0.7550224661827087)
