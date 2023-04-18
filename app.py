391321cfc8bd549afe5f8931b697e176bf17554e
Dr_Wat <50990387+DrWhats@users.noreply.github.com>
2023-04-18 20:34:37 +0500

    Added flake8 to check

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
+'''Основной файл проекта'''
 from transformers import pipeline
 import streamlit as st
 
 # import pytest
-classifier = pipeline("zero-shot-classification", model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli")
+classifier = pipeline("zero-shot-classification",
+                      model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli")
 
 text_input = st.text_input('Введите текст')
 
 
 def classify(text=None):
     """Classify the given text using a classifier"""
-    if text == None:
+    if text is None:
         user_input = input("Please enter some text to classify: ")
         text = user_input
-    candidate_labels = ["Учетная запись", "РПД", "Учебные планы", "Личный кабинет"]
-    output = classifier(text, candidate_labels, multi_label=False, use_fast=False)
+    candidate_labels = ["Учетная запись", "РПД",
+                        "Учебные планы", "Личный кабинет"]
+    output = classifier(text, candidate_labels,
+                        multi_label=False, use_fast=False)
     return output["labels"][0], output["scores"][0]
 
 
@@ -21,11 +25,3 @@ submit = st.button('Отправить')
 
 if submit:
     st.write(classify())
-
-
-# def test_classify():
-#     assert classify("Добрый день! Не могу войти в свой аккаунт. Что делать?") == ('Учетная запись', 0.7550224661827087)
-#
-#
-# #Добрый день! Не могу войти в свой аккаунт. Что делать?
-# #('Учетная запись', 0.7550224661827087)
